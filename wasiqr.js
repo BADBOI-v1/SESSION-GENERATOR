aafa92105aa4023a157066db0df26f7370622c17
BADBOI HACKER <165565859+BADBOI-v1@users.noreply.github.com>
2024-07-26 07:41:44 +0100

    Update wasiqr.js

--- wasiqr.js
+++ wasiqr.js
@@ -59,32 +59,34 @@ router.get('/', async (req, res) => {
 				   let session = await Qr_Code_By_Wasi_Tech.sendMessage(Qr_Code_By_Wasi_Tech.user.id, { text: '' + b64data });
 	
 				   let WASI_MD_TEXT = `
-*_QR Code Connected*
-*_Made With by Cod3Uchiha🤍_*
+`
+*_Qr Code Connected*
+*_Made With love by BADBOI and LORD NO NAME 🤍_*
 ______________________________________
 ╔════◇
-║ FOR ALL COD3UCHIHA BOTS
+║ FOR ALL Lord No Name Tech BOTS
 ║ _Thanks for your support._
 ╚════════════════════════╝
 ╔═════◇
 ║  『••• ⬡𝗩𝗶𝘀𝗶𝘁 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽⬡ •••』
-║⬡ *Ytube:* _youtube.com/@Cod3Uchiha_
-║⬡ *Owner:* _https://wa.me/263785028126_
+║⬡ *Owner2:* _https://wa.me/2348140825959_
+║
+║⬡ *Owner:* _https://wa.me/2348133286181_
 ║
 ║⬡MY bot Repos⬡
 ║
-║⬡ *TKM bot:* _https://github.com/Cod3Uchiha/TKM-bot_
-║⬡ *Manjiro-Sano-md:* _https://github.com/Cod3Uchiha/Manjiro-Sano-md_
+║⬡ *No name bot:* _https://github.com/Anime-King01/Devil_Cat-V2.0_
+║⬡ *No-Name-md:* _https://github.com/Anime-King01/Devil_Cat-V2.0_
 ║
 ║⬡Support channels⬡
 ║
-║⬡ *COD3UCHIHA:* _https://whatsapp.com/channel/0029VaKjSra9WtC0kuJqvl0g_
-║⬡ *TKM inc:* _https://whatsapp.com/channel/0029VadFn9M545v25zjEWI3I_
+║⬡ *LORD NO NAME:* _https://whatsapp.com/channel/0029VadIfkvFSAt7RRohyF0t_
+║⬡ *BADBOI inc:* _https://whatsapp.com/channel/0029VadCyFZGufJ2YW4bG42x_
 ║
-║⬡ *©Copyright* _Takudzwa-Mlambo|Cod3Uchiha
+║⬡ *©Copyright* _Badboi-Hacker|Lord no name 
 ╚══════════════════════════════════╝
 _____________________________________
-	
+
 _Don't Forget To Give Star To My Repo_`
 	 await Qr_Code_By_Wasi_Tech.sendMessage(Qr_Code_By_Wasi_Tech.user.id,{text:WASI_MD_TEXT},{quoted:session})
 
